eafd651e0cf68aeed5de0bd33de91d14881571a5
7mee3d <alanturing404@gmail.com>
2025-04-13 21:37:11 +0200

    1- Updating the format of print inforamtion employee using setw and stroing in tabel (styling)
    2- using memory dynamic allocation ( pointer variable )
    
    3- passing memory dynamic allocation ( pointer varaible ) to function and using dereferancing to access in value

--- 2- Memory Dynamic Allocation/Example two - create array dynamic stroing strust/Example two - create array dynamic stroing strust/Example two - create array dynamic stroing strust.cpp
+++ 2- Memory Dynamic Allocation/Example two - create array dynamic stroing strust/Example two - create array dynamic stroing strust/Example two - create array dynamic stroing strust.cpp
@@ -1,10 +1,13 @@
 ﻿#include <iostream>
 #include <string>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::setw;
+using std::left;
 
 struct Employee {
     string name{};
@@ -12,38 +15,58 @@ struct Employee {
     unsigned short int age{};
 };
 
-void fillInformationEmployee(Employee* infoEmp, int size) {
-    for (int i = 0; i < size; i++) {
+
+void fillInformationEmployee(Employee* infoEmp, int* size) {
+    cin.ignore(); 
+
+    for (int i = 0; i < *size; i++) {
         cout << "\n\nEmployee Number [ " << i + 1 << " ]\n\n";
         cout << "Enter the Name of Employee: ";
-        cin.ignore();
         std::getline(cin, (infoEmp + i)->name);
         cout << "Enter the ID of Employee: ";
         cin >> (infoEmp + i)->ID;
         cout << "Enter the Age of Employee: ";
         cin >> (infoEmp + i)->age;
+        cin.ignore(); 
     }
 }
 
-void print(Employee* infoEmp, int size) {
 
-    for (int i = 0; i < size; i++) {
-        cout << "\n\nEmployee Number [ " << i + 1 << " ]\n\n";
-        cout << "Name of Employee: " << (infoEmp + i)->name << endl;
-        cout << "ID of Employee: " << (infoEmp + i)->ID << endl;
-        cout << "Age of Employee: " << (infoEmp + i)->age << endl;
+void header() {
+    cout << endl << endl ; 
+
+    cout << "+--------+-----------------------+--------+\n";
+    cout << "|  ID    |         Name         |  Age   |\n";
+    cout << "+--------+-----------------------+--------+\n";
+}
+
+void footer() {
+    cout << "+--------+-----------------------+--------+\n";
+}
+
+void printInfoEmployee(Employee* infoEmp, int* size) {
+    header();
+
+    for (int i = 0; i < *size; i++) {
+        cout << "| " << left << setw(7) << (infoEmp + i)->ID
+            << "| " << left << setw(22) << (infoEmp + i)->name
+            << "| " << left << setw(6) << (infoEmp + i)->age << "|\n";
     }
+
+    footer();
 }
 
 int main() {
-    int size;
-    cout << "Enter the number of employees: ";
-    cin >> size;
+    int* size = new int;
 
-    Employee* array = new Employee[size];
+    cout << "Enter the number of employees: ";
+    cin >> *size;
+
+    Employee* array = new Employee[*size];
 
     fillInformationEmployee(array, size);
-    print(array, size);
+    printInfoEmployee(array, size);
 
     delete[] array;
+    delete size;
 }
