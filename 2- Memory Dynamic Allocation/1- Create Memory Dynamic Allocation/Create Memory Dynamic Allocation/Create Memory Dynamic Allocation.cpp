2d5b7c1601d6050ed6767abe95e7c28a7206efa6
7mee3d <alanturing404@gmail.com>
2025-04-13 20:24:10 +0200

    A more extended explanation of the definition line of a variable that is stored and allocated a place in memory in the runtime process and the memory dynamic allocation interpolation

--- 2- Memory Dynamic Allocation/1- Create Memory Dynamic Allocation/Create Memory Dynamic Allocation/Create Memory Dynamic Allocation.cpp
+++ 2- Memory Dynamic Allocation/1- Create Memory Dynamic Allocation/Create Memory Dynamic Allocation/Create Memory Dynamic Allocation.cpp
@@ -18,6 +18,36 @@ int main()
     int : is the data type of memory 
 
     new int return is memory address dynamicly 
+
+
+
+    // -------------------------------------
+
+    *More detailed explanation*
+
+     ( new int ) : 
+
+    *This Expression means in the spirit of telling the interpreter
+    to reserve a memory location or memory space to store the address 
+    of this variable of a type you know well.
+
+
+    *more comprehensive information
+
+    *This line tells the operating system, “Operating system,
+    reserve memory space to store only one value 
+    of the type of pointer you define.
+
+
+    //--------------------------------------------------
+
+    ( int* pointerX ) 
+    pointerX = new int  ;
+
+    *The main function of this line is to let the cursor point to the address that has been allocated
+
+
+
     */
 
 
