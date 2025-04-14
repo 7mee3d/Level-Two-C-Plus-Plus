b64ec405ff9465a47d9efe7e5bab8262bbc47351
7mee3d <alanturing404@gmail.com>
2025-04-14 13:33:33 +0200

    This code Test of My Library include the function read Element using pointer ( Memory Dynmic allocation ) ( Dynamic array )

---
+++ 2- Memory Dynamic Allocation/1- Create Memory Dynamic Allocation/Create Memory Dynamic Allocation/Memory_Dynamic_Allocation_Using_MyLibrary.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "I:\\Programming part 2\\Level Two C-Plus-Plus\3- Create My Library\\Introducation create my library\\Introducation create my library\\Test_Libaray.h"
+using namespace std; 
+
+int main() {
+	int sizeArray;
+	cout << "Enter the Size ARRAY , Sir : "; 
+	cin >> sizeArray; 
+
+	int* array; 
+	array = new int[sizeArray];
+
+	myLibReadElementsArray::readElementsInArrayToSAtoreIntegerDynmaic(array, size);
+
+}
